ca66e2c18d8558faaf54858a50190bb91bdcd432
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-08-12 19:21:43 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/products/ebanking/ApplicationEBankingConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.products.ebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationEBankingConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationEBankingConfigServerApplication.class, args);
+	}
+}
